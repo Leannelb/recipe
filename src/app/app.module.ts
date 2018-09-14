0aa777d170a6ef6c448d2885799a1e01614a8f71
Lillycode <leannelaceybyrne@outlook.com>
2018-09-13 20:46:28 -0700

    set up with all comonents and bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ShoppingListComponent } from './components/shopping/shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './components/shopping/shopping-list/shopping-list-edit/shopping-list-edit.component';
+import { RecipeComponent } from './components/recipe/recipe/recipe.component';
+import { IngredientComponent } from './components/shopping/ingredient/ingredient.component';
+import { RecipieListComponent } from './components/recipe/recipe/recipie-list/recipie-list.component';
+import { RecipieItemComponent } from './components/recipe/recipe/recipie-item/recipie-item.component';
+import { RecipieDetailComponent } from './components/recipe/recipe/recipie-detail/recipie-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+    RecipeComponent,
+    IngredientComponent,
+    RecipieListComponent,
+    RecipieItemComponent,
+    RecipieDetailComponent
   ],
   imports: [
     BrowserModule,
